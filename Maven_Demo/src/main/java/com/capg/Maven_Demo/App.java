cdbaf4f08a58aacf0ae94112cab528b2e6009c04
Tulsisethiya25 <tulsisethiya25@gmail.com>
2022-03-15 11:21:03 +0530

    second commit

---
+++ Maven_Demo/src/main/java/com/capg/Maven_Demo/App.java
@@ -0,0 +1,13 @@
+package com.capg.Maven_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
